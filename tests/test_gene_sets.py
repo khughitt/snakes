bb9ec1b7aa76919630cbe265d54aed69074e62a4
Keith Hughitt <keith.hughitt@gmail.com>
2018-11-23 09:09:05 -0500

    Gene set test naming fix

--- tests/test_gene_sets.py
+++ tests/test_gene_sets.py
@@ -8,7 +8,7 @@ from snakes import gene_sets
 # test data
 df = pd.DataFrame([[1, 2, 3], [5, 10, 15], [0, 0, 0]], index=['a', 'b', 'c'])
 
-gene_sets = {
+gsets = {
     'x': ['a', 'c', 'd'],
     'y': ['a', 'b', 'c'],
     'z': ['d', 'e', 'f']
@@ -17,5 +17,5 @@ gene_sets = {
 def test_gene_set_apply():
     expected = pd.DataFrame([[1, 2, 3], [6, 12, 18]], index=['x', 'y'])
 
-    assert_frame_equal(expected, gene_sets.gene_set_apply(df, gene_sets, 'sum'))
+    assert_frame_equal(expected, gene_sets.gene_set_apply(df, gsets, 'sum'))
 
