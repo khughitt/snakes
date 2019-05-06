93f644720e6ed7ac8ecb49e1d25c4635f245ece5
Keith Hughitt <keith.hughitt@gmail.com>
2019-05-06 18:59:07 -0400

    Added check for empty datasets to filter actions

--- snakes/filters.py
+++ snakes/filters.py
@@ -3,6 +3,7 @@ Functions for filtering datasets by row, column, or group.
 """
 import operator
 import numpy as np
+from pandas import EmptyDataError
 
 #
 # Generalized filter function
@@ -22,11 +23,16 @@ def filter_data_by_func(df, func, axis=1, op=operator.gt, value=None, quantile=N
     # apply function along rows or columns and return filtered result
     if axis == 1:
         # rows
-        return df[mask]
+        df = df[mask]
     else:
         # columns
-        return df[mask.index[mask]]
+        df = df[mask.index[mask]]
 
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+
+    return df
 #
 # Row-wise filter functions
 #
@@ -39,38 +45,73 @@ def filter_rows_by_col(df, col=None, op=operator.gt, value=None, quantile=None):
     if quantile is not None:
         value = df[col].quantile(quantile)
 
-    return df[op(df[col], value)]
+    df = df[op(df[col], value)]
+
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError
+
+    return df
 
 def filter_rows_by_na(df, op=operator.le, value=None, quantile=None):
     """Filters dataset rows based on the number of missing values"""
     if quantile is not None:
         value = df.quantile(quantile, axis=1)
 
-    return df[op(df.isnull().sum(axis=1), value)]
+    df = df[op(df.isnull().sum(axis=1), value)]
+
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+
+    return df
 
 def filter_rows_by_nonzero(df, op=operator.gt, value=None, quantile=None):
     """Filters dataset rows based on the number of 0's present"""
     if quantile is not None:
         value = df.quantile(quantile, axis=1)
 
-    return df[op((df != 0).sum(axis=1), value)]
+    df = df[op((df != 0).sum(axis=1), value)]
+
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+
+    return df 
 
 def filter_rows_col_not_na(df, col):
     """Returns all rows for which a specific column is not null"""
-    return df[df[col].notnull()]
+    df = df[df[col].notnull()]
+
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+    
+    return df
 
 def filter_rows_col_val_in(df, col, values):
     """
     Removes all rows for which a column is not one of a specified set of values
     """
-    return df[df[col].isin(values)]
+    df = df[df[col].isin(values)]
 
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+
+    return df 
 
 def filter_rows_col_val_not_in(df, col, values):
     """
     Removes all rows for which a column is one of a specified set of values
     """
-    return df[~df[col].isin(values)]
+    # check to make sure data is non-empty after filtering step
+    df = df[~df[col].isin(values)]
+
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+
+    return df
 
 def filter_rows_by_group_func(df, group, col, func, op=operator.gt, value=None, quantile=None):
     """
@@ -100,7 +141,14 @@ def filter_rows_by_group_func(df, group, col, func, op=operator.gt, value=None, 
     # get ids of rows passing the cutoff
     mask = group_stats.loc[op(group_stats, cutoff_value)].index
 
-    return df[df[group].isin(mask)]
+    # apply filter
+    df = df[df[group].isin(mask)]
+
+    # check to make sure data is non-empty after filtering step
+    if df.empty:
+        raise EmptyDataError('No data remaining after filter applied')
+
+    return df
 
 #
 # Column-wise filter functions
