aa000c51aff14406a7b03d3b86f26c2f0f371c13
Keith Hughitt <keith.hughitt@gmail.com>
2020-08-18 21:01:35 -0400

    updated test data generation script to add edge cases; missing data, zero variance rows

--- tests/generate_test_data.py
+++ tests/generate_test_data.py
@@ -10,8 +10,14 @@ the dataset dimensions and what each dimension represents.
     2. cnv (50 genes x 10 samples)
     3. response (5 drugs x 10 samples)
 
-Additionally, a GMT annotation file containing five fake gene sets is also created, as well as
-sample and drug metadata tables.
+Note that the actual values of each of the test datasets are purely random and bear no
+relation to what they are used to represent. Rather, the small datasets generated are
+mainly maint to capture the overall type (e.g. numeric matrix) of the type. This way the
+test datasets can be used as "placeholders" in simple pipelines designed to analyze
+actual data of those types, to ensure that each of the steps behaves as expected.
+
+Additionally, a GMT annotation file containing five fake gene sets are also created, as
+well as sample and drug metadata tables.
 """
 import numpy as np
 import pandas as pd
@@ -36,17 +42,27 @@ drug_ids = ['drug{:02d}'.format(x) for x in range(5)]
 
 # generate fake data and save to disk
 
-# RNA-Seq
-pd.DataFrame(np.random.randint(0, 10000, size=(50, num_samples)),
-             index = rnaseq_gene_ids, columns = sample_ids).to_csv('data/rnaseq.csv')
+# RNA-Seq (with zero variance entries)
+rna_dat = pd.DataFrame(np.random.randint(0, 10000, size=(50, num_samples)),
+             index = rnaseq_gene_ids, columns = sample_ids)
+
+rna_dat.iloc[[10, 20], ] = [0] * num_samples
+
+rna_dat.to_csv('data/rnaseq.csv')
+
 
 # CNV
 pd.DataFrame(np.round(np.random.normal(size=(30, num_samples)), 2),
              index = cnv_gene_ids, columns = sample_ids).to_csv('data/cnv.csv')
 
-# Drug AC-50
-pd.DataFrame(np.round(np.random.lognormal(mean=0, sigma=0.25, size = (5, num_samples)) * 50, 2),
-             index = drug_ids, columns = sample_ids).to_csv('data/ac50.csv')
+# Drug AC-50 (with missing values)
+drug_dat = pd.DataFrame(np.round(np.random.lognormal(mean=0, sigma=0.25, size = (5, num_samples)) * 50, 2),
+             index = drug_ids, columns = sample_ids)
+
+na_mask = nan_mat = np.random.random(drug_dat.shape) < 0.1
+drug_dat = drug_dat.mask(na_mask)
+
+drug_dat.to_csv('data/ac50.csv')
 
 # Sample metadata
 pd.DataFrame({'var1': np.random.choice([0, 1], 10),
